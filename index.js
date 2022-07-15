28d9fa96582283d02ac7fdd0a919d3b3f4ef4305
ganeshkalyan7 <ganeshkalyan506@gmail.com>
2022-07-15 21:27:42 +0530

    secound commir

--- index.js
+++ index.js
@@ -105,6 +105,9 @@ app.get("/room/:id", (req, res) => {
     }
   });
 });
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
